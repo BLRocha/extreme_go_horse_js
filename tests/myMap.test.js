0c9e2c452883262d1ff1524329de7677723d3f5d
Leandro Rocha <leandrorocha.cbrj@hotmail.com>
2019-09-26 00:21:46 -0300

    add new tests for error

--- tests/myMap.test.js
+++ tests/myMap.test.js
@@ -4,15 +4,15 @@ const { expect } = require('chai');
 require('../src/myMap');
 
 describe('Suite test for myMap >..', () => {
-	it(`[1,2,5,4].map(a => 5) expexted deep equal [6,8,10,9]`, () => {
+	it(`1 [1,2,5,4].map(a => 5) expexted deep equal [6,8,10,9]`, () => {
 		expect([1, 2, 5, 4].map(a => a + 5)).to.deep.equal([6, 7, 10, 9]);
 	});
 
-	it(`[1,2,5,4].myMap(a => 5) expexted deep equal [6,8,10,9]`, () => {
+	it(`2 [1,2,5,4].myMap(a => 5) expexted deep equal [6,8,10,9]`, () => {
 		expect([1, 2, 5, 4].myMap(a => a + 5)).to.deep.equal([6, 7, 10, 9]);
 	});
 
-	it('[1,2,5,4].myMap(a => {a + 5}) expexted deep equal [1,2,5,4]', () => {
+	it('3 [1,2,5,4].myMap(a => {a + 5}) expexted deep equal [1,2,5,4]', () => {
 		expect(
 			[1, 2, 5, 4].myMap(a => {
 				// eslint-disable-next-line no-unused-expressions
@@ -21,7 +21,7 @@ describe('Suite test for myMap >..', () => {
 		).to.deep.equal([1, 2, 5, 4]);
 	});
 
-	it('[].myMap(a => 5) expexted deep equal [empty]', () => {
+	it('4 [].myMap(a => 5) expexted deep equal [empty]', () => {
 		expect(
 			[].myMap(a => {
 				// eslint-disable-next-line no-unused-expressions
@@ -29,7 +29,7 @@ describe('Suite test for myMap >..', () => {
 			})
 		).to.deep.equal([]);
 	});
-	it('[].myMap() expexted deep equal [empty]', () => {
+	it('5 [].myMap() expexted deep equal [empty]', () => {
 		expect([1, 2, 3, 5].myMap()).to.throw(
 			TypeError,
 			'MyMap(function(a,b,c) {}), callback required a function'
