f53b7c3e6db1f84b63cb674aebe6e7801262211f
Leandro Rocha <leandrorocha.cbrj@hotmail.com>
2019-09-25 23:40:45 -0300

    add function for error

--- src/myMap.js
+++ src/myMap.js
@@ -11,9 +11,11 @@ module.exports = Array.prototype.myMap = function(callback) {
 	const nArr = [];
 	if (this.length === 0) return [...this];
 	if (typeof callback !== 'function')
-		return new TypeError(
-			'MyMap(function(a,b,c) {}), callback required a function'
-		);
+		return function() {
+			throw new TypeError(
+				'MyMap(function(a,b,c) {}), callback required a function'
+			);
+		};
 	for (let i = 0; i < this.length; i++) {
 		const a = callback(arr[i], i, this);
 		if (a === undefined) {
