acd8cc4dd9d12287bb0cdf245d321706669ac679
Leandro Rocha <leandrorocha.cbrj@hotmail.com>
2020-10-15 19:37:54 -0300

    add test suite for myFilter

--- tests/src.test.js
+++ tests/src.test.js
@@ -2,6 +2,7 @@
 require('../src/myMap');
 require('../src/myEvery');
 require('../src/mySome');
+require('../src/myFilter');
 
 const { expect } = require('chai');
 
@@ -73,5 +74,19 @@ describe('Suite test for mySome >..', () => {
 		expect([1, 2, '5', 4].mySome(a => typeof a === 'string')).to.deep.equal(
 			true
 		);
+	});
+});
+
+describe("Suite test for myFilter >..", () => {
+	const filtTets = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+	it('filtTest.myFilter((a => a % 2 == 0)); Expected [2,4,6,8,10,12,14,16,18,20]', () => {
+		expect(filtTets.myFilter(a => a % 2 == 0)).to.deep.equal([2,4,6,8,10,12,14,16,18,20]);
+	});
+	it('filtTest.myFilter(); Expected [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]', () => {
+		expect(filtTets.myFilter()).to.deep.equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
+	});
+
+	it('filtTest.myFilter( a => a % 2 == 8 ); Expected []', () => {
+		expect(filtTets.myFilter( a => a % 2 == 8 )).to.deep.equal([]);
 	});
 });
