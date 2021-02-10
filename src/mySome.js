3ca921da772f3657795404e7689e6f6c70d965e3
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2021-02-09 22:04:17 -0300

    rename variables

--- src/mySome.js
+++ src/mySome.js
@@ -8,10 +8,10 @@
 
 /* eslint-disable-next-line no-multi-assign */
 module.exports = Array.prototype.mySome = function(callback) {
-	if (this.length === 0) return false;
+	if (!this.length) return false;
 	for (let i = 0; i < this.length; i++) {
-		let a = callback(this[i], i, this);
-		if (a === true) {
+		let currentValue = callback(this[i], i, this);
+		if (currentValue === true) {
 			return true;
 		}
 	}
