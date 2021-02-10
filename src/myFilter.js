73113bbafd149f5c81300a030479b3ec76430618
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2021-02-09 22:06:59 -0300

    rename variables

--- src/myFilter.js
+++ src/myFilter.js
@@ -1,13 +1,13 @@
-
 module.exports = Array.prototype.myFilter = function(filter) {
-    if (typeof filter == 'function') {
-        const newArray = [];
-        let acc = 0;
-        this.forEach((condicao) => {
-            if (filter(condicao)) {
-                newArray[acc++] = condicao
-            }
-        });
-        return newArray;
-    } return this;
-}
+	if (typeof filter === 'function') {
+		const newArray = [];
+		let acc = 0;
+		this.forEach(condition => {
+			if (filter(condition)) {
+				newArray[acc++] = condition;
+			}
+		});
+		return newArray;
+	}
+	return [...this];
+};
