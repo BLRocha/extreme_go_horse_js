a0e5055b0525613d8f285e6f243dedc644ac21f0
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2021-04-06 14:42:11 -0300

    ESlint fix indentations

--- tests/myIndexOf.test.js
+++ tests/myIndexOf.test.js
@@ -6,11 +6,15 @@ require('../src/myIndexOf');
 
 describe('Suite test for myIndexOf >..', () => {
 	it(`[45,8,7,5,6,8,5,12,48,46,13,25,17,89].myIndexOf(48)`, () => {
-		expect([45,8,7,5,6,8,5,12,48,46,13,25,17,89].myIndexOf(46)).to.be.equal(9);
+		expect(
+			[45, 8, 7, 5, 6, 8, 5, 12, 48, 46, 13, 25, 17, 89].myIndexOf(46)
+		).to.be.equal(9);
 	});
-	
+
 	it(`[45,8,7,5,6,8,5,12,48,46,13,25,17,89].myIndexOf(10)`, () => {
-		expect([45,8,7,5,6,8,5,12,48,46,13,25,17,89].myIndexOf(10)).to.be.equal(-1);
+		expect(
+			[45, 8, 7, 5, 6, 8, 5, 12, 48, 46, 13, 25, 17, 89].myIndexOf(10)
+		).to.be.equal(-1);
 	});
 	it(`[].myIndexOf(48)`, () => {
 		expect([].myIndexOf(46)).to.be.equal(-1);
